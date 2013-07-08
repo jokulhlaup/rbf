c7739b4afedc7f560689fd136d59897b853a8153
jokulhlaup <mhay@uw.edu>
2013-07-08 16:52:07 -0700

    jefferys works

--- jeff.jl
+++ jeff.jl
@@ -1,3 +1,5 @@
+module jefferys
+export solveJefferys,ode4u
 #Modification of ODE4 from package ODE
 function ode4u{T}(F::Function, h::Float64,n::Int64, x0::AbstractVector{T})
     x = Array(T, (length(tspan), length(x0)))
@@ -6,26 +8,34 @@ function ode4u{T}(F::Function, h::Float64,n::Int64, x0::AbstractVector{T})
     midxdot = Array(T, (4, length(x0)))
     for i = 1:n
         # Compute midstep derivatives
-        midxdot[1,:] = F(tspan[i], x[i,:]')
-        midxdot[2,:] = F(tspan[i]+h/2, x[i,:]' + midxdot[1,:]'*h/2)
-        midxdot[3,:] = F(tspan[i]+h/2, x[i,:]' + midxdot[2,:]'*h/2)
-        midxdot[4,:] = F(tspan[i]+h, x[i,:]' + midxdot[3,:]'*h)
+        midxdot[1,:] = F(x[i,:]')
+        midxdot[2,:] = F(x[i,:]' + midxdot[1,:]'*h/2)
+        midxdot[3,:] = F(x[i,:]' + midxdot[2,:]'*h/2)
+        midxdot[4,:] = F(x[i,:]' + midxdot[3,:]'*h)
 
         # Integrate
         x[i+1,:] = x[i,:] + 1./6.*h[i].*[1 2 2 1]*midxdot
     end
-    return (tspan, x)
+    return x
 end
 
 
 #Using ODE
-function solveJefferys(vort::Array{T,2},epsdot:Array{T,2},theta,phi,dt,n)
-  n=(theta,phi)->[sin(theta)*cos(phi),sin(theta)*sin(phi),cos(theta)]
+function solveJefferys{T}(vort,epsdoy,theta,phi,dt,n)
+  n=theta->[sin(theta[1])*cos(theta[2]),sin(theta[1])*sin(theta[2]),cos(theta[1])]
 
-  f=(theta,phi)->vort[2:3,:]*n(theta,phi)[2:3]-(epsdot[2:3,:]*n(theta,phi)[2:3]-(n(theta,phi)*epsdot*n)*n(theta,phi) [2:3])
-  
+  f=theta->((vort*n(theta))[2:3])-((epsdot*n(theta))[2:3]-(n(theta)'*epsdot*n(theta))[0]*n(theta)[2:3])
+  n=jefferys.ode4u(f,dt/4,4,theta)
+  theta[1]=acos(lhs[3])
+  theta[2]=asin(lhs/(asin(theta)))
+  return theta
+end
 
+end
 
+function foo(theta:)
+((vort*n(theta))[2:3])-((epsdot*n(theta))[2:3]-(n(theta)'*epsdot*n(theta))*n(theta)[2:3]) 
+end
 
 
 
