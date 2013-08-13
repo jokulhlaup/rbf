d8903bdac6ad943e9f765d4f72187e3e5d3a23c4
jokulhlaup <mhay@uw.edu>
2013-08-13 15:14:58 -0700

    did some stuff

--- Flow.jl
+++ Flow.jl
@@ -26,6 +26,15 @@ function imq(x,x0,ep=1::Number)
   r2=sum([x-x0].*[x-x0])
   return (1/sqrt(1+ep^2*r2))
   end
+
+function imq(r,ep=1::Number)
+  return 1/sqrt(1+r.^2*ep.^2)
+  end
+
+function lapIMQ(r,ep=1::Number)
+  return ep^.2*(ep.^2*r.^2-2)./(ep.^2*r.^2+1).^2.5
+  end
+
 function d2imq(x,x0,i::Int,j::Int,eps::Number)
   r=x-x0
   r2=(r*r')[1]
@@ -35,6 +44,19 @@ function d2imq(x,x0,i::Int,j::Int,eps::Number)
       return eps*(eps*(3*r2[i]-sum(r2))-1)/(eps*r2+1)^2.5
     end
   end
+
+function lan(x0,x,C,ep)
+  a=[:1,:2,:3,:3,:3,:2]
+  for k=1:6
+    u[k]=:(d2imq(x,x0,$(a[$k]),j,ep))
+    end
+  for k=1:6
+    d[k]=:(sum(C[:,$k].*u))
+    end
+  for i=1:3
+    L
+  for i=1:6
+    u[i]=d2imq(x,x0,i,
 
 function creatDict(xs) #input of list of points
   n=length(xs[1,:])
@@ -70,14 +92,14 @@ function genrSystem(fpar::FlowParams)
 
 #d is a n-length 1d array of distances,
 #where a[i]=dist(starred pt, pt i)
-function getWeights(kd,coors,L::Function,bnd_index::Int,n::Int,nnn::Int)
+function getWeights(kd::PyObject,coors,L::Function,bnd_index::Int,n::Int,nnn::Int)
   (d,inds0)=kd[:query](coors[1:bnd_index-1,:],nnn)
   inds=inds0+1
   w=Array(Float64,bnd_index-1,nnn)
   S=Array(Float64,nnn,nnn)
   for i=1:bnd_index-1
     #generate the weights matrix
-    S=Float64[imq(coors[j,:],coors[k,:],0.1) for j in inds[i,:],k in inds[i,:]]
+    S=Float64[imq(d[i,:],0.1)]
     Lh=L(d[i,:])
     #generate the augmented matrix
     S=[S ones(nnn)
@@ -87,6 +109,34 @@ function getWeights(kd,coors,L::Function,bnd_index::Int,n::Int,nnn::Int)
     end
   return (w,d,inds)
   end
+
+#getweights for 3 dep vars 
+function getWeights(kd::PyObject,coors,C,L::Function,bnd_index::Int,n::Int,nnn::Int)
+  (d,spinds0)=kd[:query](coors[1:bnd_index-1,:],nnn)
+  spinds=spinds0+1
+  #Move from spatial to spatial/component indexing
+  #i where i mod 3 == 1 is x component of spinds[i], ' '==2 is y, etc
+  bin=3*bnd_index
+  nnnc=3*nnn
+
+  inds=Array(Float64,bin-3,nnnc)
+  w=Array(Float64,bin-3,nnnc)
+  S=Array(Float64,nnnc,nnnc)
+  for i=1:bnd_index-1
+    #generate the weights smatrix
+    S=Float64[imq(d[i,:],0.1)]
+    Lh=L(d[i,:])
+    #generate the augmented matrix
+    S=[S ones(nnn)
+       ones(nnn)' 0]
+    Lh=[Lh',0]
+    w[i,:]=(S\Lh)[1:nnn]
+    end
+  return (w,d,inds)
+  end
+
+
+
 
 
 #for dirichletBCs
