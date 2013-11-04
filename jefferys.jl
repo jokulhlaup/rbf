cec834f08109787cb84cfb071661bd0c16aefa92
Mike Hay <mhay@uw.edu>
2013-11-04 11:23:09 -0800

    nothing

--- jefferys.jl
+++ jefferys.jl
@@ -1,6 +1,7 @@
 module jefferys
 using ODE
-export Fabric,Fabric2,GlobalPars,AbstractFabric,solveJefferys,rk4,nRK4,rotC,jefferysRHS,fabricHelper
+export Fabric,Fabric2,GlobalPars,AbstractFabric
+export solveJefferys,rk4,nRK4,rotC,jefferysRHS,fabricHelper
 ##########################
 ##########Get viscosity###
 ##########################
@@ -97,9 +98,12 @@ function rotC(R)
   K3 = [ R[2,1]*R[3,1] R[2,2]*R[3,2] R[2,3]*R[3,3] ; 
          R[3,1]*R[1,1] R[3,2]*R[1,2] R[3,3]*R[1,3] ; 
          R[1,1]*R[2,1] R[1,2]*R[2,2] R[1,3]*R[2,3] ] ;
-  K4 = [ R[2,2]*R[3,3]+R[2,3]*R[3,2] R[2,3]*R[3,1]+R[2,1]*R[3,3] R[2,1]*R[3,2]+R[2,2]*R[3,1] ; 
-         R[3,2]*R[1,3]+R[3,3]*R[1,2] R[3,3]*R[1,1]+R[3,1]*R[1,3] R[3,1]*R[1,2]+R[3,2]*R[1,1] ;       
-         R[1,2].*R[2,3]+R[1,3].*R[2,2] R[1,3].*R[2,1]+R[1,1].*R[2,3] R[1,1].*R[2,2]+R[1,2].*R[2,1]] ; 
+  K4 = [ R[2,2]*R[3,3]+R[2,3]*R[3,2] R[2,3]*R[3,1]+R[2,1]*R[3,3] 
+         R[2,1]*R[3,2]+R[2,2]*R[3,1] ; 
+         R[3,2]*R[1,3]+R[3,3]*R[1,2] R[3,3]*R[1,1]+R[3,1]*R[1,3] 
+         R[3,1]*R[1,2]+R[3,2]*R[1,1] ;       
+         R[1,2].*R[2,3]+R[1,3].*R[2,2] R[1,3].*R[2,1]+
+         R[1,1].*R[2,3] R[1,1].*R[2,2]+R[1,2].*R[2,1]] ; 
   K = [ K1  2*K2 ; 
         K3   K4   ] ;
   C = zeros(6,6)
@@ -107,34 +111,22 @@ function rotC(R)
   C = K * C * transpose(K) 
 end
 
-#gets the rotation matrices
-function getRotM(fab::AbstractFabric)
-  R=Array(Float64,fab.ngr,3,3)
-  for i=1:fab.ngr*fab.ns
-    A=[0 0 fab.p[1,i]
-       0 0 -fab.p[2,i] 
-       -fab.p[1,i] fab.p[2,i] 0]
-    A2=[-fab.p[2,i]^2 fab.p[2,i]*fab[1,i] 0
-        fab.p[2,i]*fab[1,i] fab.p[2,i]^2 0 
-        0 0 fab.p[1,i]^2+fab.p[2,i]^2]
-    R[:,:,i]=sin(acos(fab.p[3,i]))*A+(1-fab.p[3,i])*A2
-    R[1,1,i]+=1;R[2,2,i]+=1;R[3,3,i]+=1
-    end
-  return R
-  end
-#this is the closure that returns fabEvolve!, which evolves the fabric based on the timestep.
+#this is the closure that returns fabEvolve!,
+#which evolves the fabric based on the timestep.
 function fabricHelper(pars::GlobalPars,fab::AbstractFabric,f::Function)
   #this is the function that actually does the rotation.  
   function fabEvolve!(pars::GlobalPars,fab::Fabric,f) #jefferys equation
     for i=1:fab.ns
-      fab.p[:,i,:]=nRK4(f,fab.ngr,fab.h,pars.nrk,pars.dt,fab.p[:,i,:],fab.vort[:,:,i],fab.epsdot[:,:,i])
+      fab.p[:,i,:]=nRK4(f,fab.ngr,fab.h,pars.nrk,pars.dt,fab.p[:,i,:],
+          fab.vort[:,:,i],fab.epsdot[:,:,i])
       end
       return fab.p
     end
 
   function fabEvolve!(pars::GlobalPars,fab::Fabric2,f) #jefferys equation
     for i=1:fab.ns
-      fab.p[:,i,:]=nRK4(f,fab.ngr,fab.h,pars.nrk,pars.dt,fab.p[:,i,:],fab.vort[:,:,i],fab.epsdot[:,:,i])
+      fab.p[:,i,:]=nRK4(f,fab.ngr,fab.h,pars.nrk,pars.dt,fab.p[:,i,:],
+          fab.vort[:,:,i],fab.epsdot[:,:,i])
       end
       dynRextal!(fab::Fabric2)
       return fab.p
@@ -152,6 +144,26 @@ function fabricHelper(pars::GlobalPars,fab::AbstractFabric,f::Function)
     return fabEvolve!
   end
 
+#gets the rotation matrices
+function getRotMHelper(Array{T,1},A::Array{Float64,2},
+    A2=Array{Float64,2},R::Array{Float64,2})
+  A=[0 0 p[1]
+     0 0 p[2] 
+     -p[1] p[2] 0]
+  A2=[-p[2]^2 p[2]*fab[1] 0
+      p[2]*p[1] p[2]^2 0 
+      0 0 p[1]^2+p[2]^2]
+  R[:,:]=sin(acos(p[3]))*A+(1-p[3])*A2
+  R[1,1]+=1;R[2,2]+=1;R[3,3]+=1
+  return R
+  end
+
+function getC(fab::AbstractFabric,A::Array{Float64,2},
+    A2=Array{Float64,2},R::Array{Float64,2})
+  for i=1:fab.ns*fab.ngr
+    getRotMHelper(fab.p[3*i-2:3*i],A,A2,R)
+    end
+  end
 
 #Main driver routine to get the viscosity.
 #refactor this by 
@@ -162,14 +174,14 @@ function getVisc!(fab::AbstractFabric,pars::GlobalPars,fabEvolve::Function)
   #advance the viscosity
   #get new theta
   fabEvolve!(fab,pars)
+  R=Array(Float64,3,3)
+  A=Array(Float64,3,3)
+  A2=Array(Float64,3,3)
   #get the rotation matrices for each theta pair
-  R=getRotM(fab)
+  getC!(fab,A,A2,R)
   #now rotate each grain's C matrix (C_ij= delta5,5)
   #Then invert it to get visc. yay.
-  for i=1:fab.ngr
-    C[:,:,i]=rotC(R[:,:,i])
-    end
-  aC=mean(C,1) #check if right
+  aC=mean(C,3) #check if right
   fab.visc=inv(aC)
   end
 
