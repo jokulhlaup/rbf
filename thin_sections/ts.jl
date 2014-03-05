c59ea8c0707ae6eab8fb4d6306323f9cec6925ba
Mike Hay <mhay@uw.edu>
2014-03-05 10:45:49 -0800

    added thinning function

--- thin_sections/ts.jl
+++ thin_sections/ts.jl
@@ -1,5 +1,43 @@
 using Grid Tests
 
+function wrapper(ages,par,ts_svs,fab,pars,jefferysRHS)
+  t_c=p[1]
+  fab.epsdot[1,1]=par[2]
+  fab.epsdot[2,2]=par[3]
+  fab.epsdot[3,3]=par[4]
+  fab.epsdot[2,3]=par[5]
+  fab.epsdot[1,3]=par[6]
+  fab.epsdot[1,2]=par[7]
+  symmetrize!(fab.epsdot)
+  fab.vort[3,2]=-par[8]
+  fab.vort[2,3]=par[8]
+  fab.epsdot[2,3]=par[8]
+  fab.epsdot[3,2]=par[8]
+  function objective(p)
+    sv=Array(Float64,0)
+    dages=ages[2:end]-ages[1:end-1]
+    for i=1:length(dages)
+      pars.dt=t_c*dage
+      fabE(pars,fab,jefferysRhs)
+      sv[:,i]==svd(fab.p[:,:,1])[2]
+      end
+    sse=sum((ts_svs-sv).^2)
+    return sse
+    end
+  return objective
+  end
+  
+function readif(rf)
+  c=Array(Float64,0)
+  for i=1:length(rf)
+    try  append!(c,float64(split(replace(rf[i],r"[\t\r]","\r"))[2:3]))  #append!(c,float64(split(rf[i],'\t')[2:3]))
+      catch
+        continue
+      end
+    end
+  return reshape(c,(2,int(length(c)/2)))
+  end
+
 cd("thin_sections/C-axisdatabase")
 dr=readdir()
 c=Dict()
@@ -21,18 +59,6 @@ for d=1:length(dr)
   cd("..")
   end
 cd("..")
-function readif(rf)
-  c=Array(Float64,0)
-  for i=1:length(rf)
-    
-    try  append!(c,float64(split(replace(rf[i],r"[\t\r]","\r"))[2:3]))  #append!(c,float64(split(rf[i],'\t')[2:3]))
-      catch
-        continue
-      end
-    end
-  return reshape(c,(2,int(length(c)/2)))
-  end
-
 
 #now interpolate depth-age
 dr=sort(float(dr))
@@ -54,35 +80,15 @@ dages=ages[2:end]-ages[1:end-1]
     sv[:,i]==svd(fab.p[:,:,1])[2]
     end
 
+function thinning(dages,
 
+#get thinning function -> strain rate
+thinning = d layer thickness /dt = dage
+    layer thickness = dz/dage
+d layer thickness/dt(age)=
+obj=wrapper(ages,par,ts_svs,fab,pars,jefferysRHS)
 
-function wrapper(ages,p,ts_svs,fab,pars,jefferysRHS)
-  t_c=p[1]
-  fab.epsdot[1,1]=p[2]
-  fab.epsdot[2,2]=p[3]
-  fab.epsdot[3,3]=p[4]
-  fab.epsdot[2,3]=p[5]
-  fab.epsdot[1,3]=p[6]
-  fab.epsdot[1,2]=p[7]
-  symmetrize!(fab.epsdot)
-  fab.vort[3,2]=-p[8]
-  fab.vort[2,3]=p[8]
-  fab.epsdot[2,3]=p[8]
-  fab.epsdot[3,2]=p[8]
-  function objective(ages,p)
-    sv=Array(Float64,0)
-    dages=ages[2:end]-ages[1:end-1]
-    for i=1:length(dages)
-      pars.dt=t_c*dage
-      fabE(pars,fab,jefferysRhs)
-      sv[:,i]==svd(fab.p[:,:,1])[2]
-      end
-    sse=sum((ts_svs-sv).^2)
-    return sse
-    end
-  return objective
-  end
-      
+    
        
 #get singular values and directions
 
